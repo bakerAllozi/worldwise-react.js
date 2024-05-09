81aba4bb6040468ab2aea89c285ce7325986da98
Baker-Allozi <broomogf@gmail.com>
2024-05-09 22:01:55 +0300

    Updates

---
+++ assets/PageNotFound-e71cd2ec.js
@@ -0,0 +1,1 @@
+import{j as n}from"./index-24c979d4.js";function t(){return n.jsx("div",{children:n.jsx("h1",{children:"Page not found 😢"})})}export{t as default};
--- assets/PageNotFound-ef4473a4.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./index-ee0615b8.js";function t(){return n.jsx("div",{children:n.jsx("h1",{children:"Page not found 😢"})})}export{t as default};
